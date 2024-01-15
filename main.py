584387c4beb919fdf8284ce3a6eafec6e793bed2
Aziz Chikh <azizchpay99@outlook.com>
2024-01-15 02:10:45 +0100

    Implement the UI with streamlit + The chatbot is functioning correctly

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from openai import OpenAI
+import streamlit as st
 
 # It is prefered to add the openai key as an environment variable!
 # Since the assignment requires the direct insertion of the api key, I will do it this way
-
 OPENAI_API_KEY = "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"
-HISTORY = []
 TASK = "You are a gentleman and your task is to answer the question in the most kind, gentle and funny way."
 
 client = OpenAI(api_key=OPENAI_API_KEY)
@@ -20,35 +19,56 @@ def generate_chat_gpt(user_input, final_prompt=TASK):
     max_tokens=400,
     temperature=0.6
     )
-    #completion = str(response)
 
     return completion
 
+def run_chatbot_ui():
+    st.set_page_config(
+    page_title="Gentle Chatbot",
+    layout="wide",
+    page_icon="🤵"
+)
+
+    #st.title("Chat with AI ")
+    st.title("Chat with Gentle Chatbot 🤵🤖")
+
+    st.sidebar.text("Instructions:")
+    st.sidebar.markdown("- Type your message in the input box.")
+    st.sidebar.markdown("- The chatbot will respond kindly and gently.")
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "system",
+            "content": "Hello, feel free to ask anything!"}
+        ]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    user_prompt = st.chat_input()
+
+    if user_prompt is not None:
+        st.session_state.messages.append(
+            {"role": "user",
+            "content": user_prompt}
+        )
+        with st.chat_message("user"):
+            st.write(user_prompt)
+
+        if st.session_state.messages[-1]["role"] != "system":
+            with st.chat_message("system"):
+                with st.spinner("Loading ..."):
+                    user_input = st.session_state.messages[-1]["content"]
+                    history_prompt = f"Here's the last conversation with the user:\n User: {user_input}\n Chatbot: {st.session_state.messages[-2]['content']} \n {TASK}"
+                    ai_response = generate_chat_gpt(user_input=user_prompt, final_prompt=history_prompt).choices[0].message.content
+                    st.write(ai_response)
+            new_ai_message = {"role": "system",
+                            "content": ai_response}
+
+            st.session_state.messages.append(new_ai_message)
 
 if __name__=="__main__":
-    print("Setup works!")
-
-    #res = generate_chat_gpt(user_input="Am I good enough to achieve my dreams?")
-    #res = generate_chat_gpt(user_input="What is my name?")
-
-    #Access the reponse
-    #print(res.choices[0].message.content)
-    #print(res)
-    print("Hello, how can I assist you?")
-    history_prompt = ""
-    i=0
-    while True:
-        print(i)
-        user_input = input('User: ')
-        if history_prompt == "":
-            completion = generate_chat_gpt(user_input=user_input)
-            history_prompt = f"Here's the last conversation with the user:\n User: {user_input}\n Chatbot: {completion.choices[0].message.content} \n {TASK}"
-            #print("history EMPTY")
-        else:
-            completion = generate_chat_gpt(user_input=user_input, final_prompt=history_prompt)
-            print(history_prompt+ "\n\n")
-            history_prompt = f"Here's the last conversation with the user:\n User: {user_input}\n Chatbot: {completion.choices[0].message.content} \n {TASK}"
-            #print("HISTORY ELSE")
-        print(completion.choices[0].message.content)
-        i += 1
+    
+    run_chatbot_ui()
 
